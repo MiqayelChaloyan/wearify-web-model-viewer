0aab986df3325cdec41b25cbc1e8cdda419f8460
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-05-21 14:04:53 +0400

    update deepar link

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 // import modelPath from '../assets/model/glasses.png';
 
 // GitHub link
-const hrefGitHub = 'https://nazeli-terpetrosyan.github.io/bows/';
+const hrefGitHub = 'https://wearifyai.github.io/ARTryOn/?effect=bows';
 
 // Google Drive images links
 const logoUrl = 'https://drive.google.com/thumbnail?id=1vT4xy5UQYQ4s4AwPdnliDVkk6LKJvDw2';
